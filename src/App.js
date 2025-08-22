06d6b9072b1c9bb0d90d0e1047f4fdb2144944d5
Romina <rominahelou@gmail.com>
2025-08-22 13:27:22 +0200

    Creating TicketList component and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css'
-import { useReducer } from 'react';
-import TicketForm from './components/TicketForm';
-import ticketReducer from './reducers/ticketReducer';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import { useReducer } from "react";
+import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
+  const initialState = { tickets: [] };
 
-  const initialState = {tickets: []}
-
-  const [state, dispatch] = useReducer(ticketReducer, initialState)
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
 
   return (
     <div className="App">
-     <div className='container'>
-      <h1>Bug Blaster</h1>
-      <TicketForm dispatch={dispatch}></TicketForm>
-     </div>
+      <div className="container">
+        <h1>Bug Blaster</h1>
+        <TicketForm dispatch={dispatch}></TicketForm>
+
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All tickets</h2>
+
+            <TicketList
+              tickets={state.tickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/TicketItem.js
+++ src/components/TicketItem.js
@@ -13,7 +13,7 @@ export default function TicketItem({ticket, dispatch}) {
     return (
 
         <div className="ticket-item">
-            <div className={`priority-dot ${priorityClass[ticket-priority]} `}></div>
+            <div className={`priority-dot ${priorityClass[ticket.priority]} `}></div>
 
         <h3>{title}</h3>
         <p>{description}</p>
